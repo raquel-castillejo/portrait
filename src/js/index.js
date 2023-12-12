ee347da6ac4b3c95581e0b7c583a77a82dcd7eb6
Raquel Castillejo <125091177+raquel-castillejo@users.noreply.github.com>
2023-12-12 21:16:11 +0100

    files

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
+/*
+menu principal: instrucciones y personajes
 
+click sobre personaje -> pagina de personaje
+
+aparece el personaje con cara 
+tras 5 segundos se quita la cara
+aparecen las piezas
+
+click sostenido sobre una pieza
+la pieza se mueve con el raton
+al soltar, la pieza queda donde soltaste -> tiene que ser sobre el dibujo, 
+si no, vuelve a la posicion original o no se suelta
+
+click sobre boton de terminado
+
+calcular la posicion de la pieza 
+comparar con la posicion correcta de la pieza
+asignar un porcentaje de acierto/error
+
+mostrar el resultado final en pantalla
+
+aparece boton volver a jugar
+*/
